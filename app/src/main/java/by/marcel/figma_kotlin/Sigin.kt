b4953024209bad294cb8770a2d09f0716f02c60c
fito <fitto martcellinod>
2023-12-05 20:49:40 +0700

    Project MoProg Apps Monas

---
+++ app/src/main/java/by/marcel/figma_kotlin/Sigin.kt
@@ -0,0 +1,11 @@
+package by.marcel.figma_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sigin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigin)
+    }
+}
---
+++ app/src/main/java/by/marcel/figma_kotlin/SignUp.kt
@@ -0,0 +1,11 @@
+package by.marcel.figma_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
